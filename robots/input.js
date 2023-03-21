cbea25f057846591e462785af251b8509a41116b
Roberto Arruda <roberto0arruda@hotmail.com>
2023-03-21 17:49:40 -0400

    feat: add load cache

--- robots/input.js
+++ robots/input.js
@@ -8,7 +8,7 @@ async function robot() {
   }
 
   content.searchTerm = await askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
+  content.prefix = askAndReturnPrefix(content.searchTerm)
   state.save(content)
 
   async function askAndReturnSearchTerm() {
@@ -25,9 +25,9 @@ async function robot() {
     return selectedTrendText
   }
 
-  function askAndReturnPrefix() {
+  function askAndReturnPrefix(searchTerm) {
     const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, `Choose an option for ${searchTerm}: `)
     const selectedPrefixText = prefixes[selectedPrefixIndex]
 
     return selectedPrefixText
@@ -35,8 +35,16 @@ async function robot() {
 
   async function googleTrendsRss() {
     const parser = new rssparser()
+    let rss
 
-    let rss = await parser.parseURL('https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR')
+    try {
+      rss = state.load('daily')
+      console.log('Load CACHE...')
+    } catch (e) {
+      console.log('Please Wait...')
+      rss = await parser.parseURL('https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR')
+      state.saveDailyRss(rss)
+    }
 
     return rss.items.map(item => item.title)
   }
